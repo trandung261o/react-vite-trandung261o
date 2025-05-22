1fa48a5a9d2fca123f7439ea9650d080ac15115c
Tran Dinh Dung <trandungtk321@gmail.com>
2025-05-23 00:42:58 +0700

    video 30

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,6 +1,11 @@
+import './style.css'
+
 const MyComponent = () => {
     return (
-        <div> eric & hoidanit </div>
+        <>
+            <div> eric & hoidanit </div>
+            <div className="child" style={{ borderRadius: "10px" }}>child</div>
+        </>
     );
 }
 
