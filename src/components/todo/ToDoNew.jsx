da10a5796e47eefc2b79fbfd495dbf9d4ecda585
Tran Dinh Dung <trandungtk321@gmail.com>
2025-05-26 23:10:43 +0700

    video 36

--- src/components/todo/ToDoNew.jsx
+++ src/components/todo/ToDoNew.jsx
@@ -4,10 +4,23 @@ const ToDoNew = (props) => {
 
     // addNewTodo("eric")
 
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name)
+    }
+
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
