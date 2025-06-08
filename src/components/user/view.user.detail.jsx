9e5c8f29cf25aff705fe58ad00d872d170e06074
Tran Dinh Dung <trandungtk321@gmail.com>
2025-06-08 15:57:47 +0700

    video 79

---
+++ src/components/user/view.user.detail.jsx
@@ -0,0 +1,37 @@
+import { Drawer } from "antd";
+
+const ViewUserDetail = (props) => {
+    const {
+        dataDetail,
+        setDataDetail,
+        isDetailOpen,
+        setIsDetailOpen
+    } = props;
+
+    return (
+        <Drawer title="Chi tiet User"
+            onClose={() => {
+                setDataDetail(null)
+                setIsDetailOpen(false)
+            }}
+            open={isDetailOpen}
+        >
+            {dataDetail ? <>
+                <p>Id: {dataDetail._id}</p>
+                <br />
+                <p>Full name: {dataDetail.fullName}</p>
+                <br />
+                <p>Email: {dataDetail.email}</p>
+                <br />
+                <p>Phone number: {dataDetail.phone}</p>
+            </>
+                :
+                <>
+                    <p>Không có dữ liệu</p>
+                </>
+            }
+        </Drawer>
+    )
+}
+
+export default ViewUserDetail;
