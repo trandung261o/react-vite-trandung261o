7301240345ea6ac6eb0eb07b0d1a2817e51c1340
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-01 15:22:06 +0700

    hoc lai

--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import LoginPage from './pages/login.jsx';
-import RegisterPage from './pages/register.jsx';
-import UserPage from './pages/user.jsx';
-import BookPage from './pages/book.jsx';
-import './styles/global.css'
-import TodoApp from './components/todo/TodoApp.jsx';
-import ErrorPage from './pages/error.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <TodoApp />
-      },
-      {
-        path: "/users",
-        element: <UserPage />
-      },
-      {
-        path: "/books",
-        element: <BookPage />
-      }
-    ]
-  },
-  {
-    path: "/login",
-    element: <LoginPage />
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />
-  }
-]);
-
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <RouterProvider router={router} />
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
