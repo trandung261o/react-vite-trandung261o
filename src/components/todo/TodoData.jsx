24339bf902de60da82cf16df42171c6d9bbefef1
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-02 21:58:18 +0700

    video 34

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,21 @@
 
-const TodoData = () => {
+const TodoData = (props) => {
+    /**
+     * prop la mot object {}
+     * {
+     *      name: "Eric",
+     *      age: 25,
+     *      data: {}
+     * }
+     */
+
+    //object destructuring
+    const { name, age, data } = props;
+
+    console.log(">>> check props:", props);
     return (
         <div className='todo-data'>
+            <div>My name is {name}</div>
             <div>Learning React</div>
             <div>Watching youtube</div>
         </div>
