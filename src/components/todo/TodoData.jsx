b11f286c4649e25c81baf3a89aa88ed39883292a
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-02 21:22:25 +0700

    video 33

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,11 @@
+
+const TodoData = () => {
+    return (
+        <div className='todo-data'>
+            <div>Learning React</div>
+            <div>Watching youtube</div>
+        </div>
+    );
+}
+
+export default TodoData;
