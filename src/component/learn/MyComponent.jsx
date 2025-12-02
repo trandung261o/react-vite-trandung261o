d56658db7c3cae992472fc25bae2d5b934b3da84
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-02 17:01:31 +0700

    video 31

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -4,9 +4,22 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const hoidanit = "eric"      //string
+    // cosnt hoidanit = 25          //number
+    // const hoidanit = true;       //boolean
+    // const hoidanit = undefined; 
+    // const hoidanit = null;
+
+    const hoidanit = [1, 2, 3];
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 25
+    // };
+
     return (
         <>
-            <div> eric & hoidanit update </div>
+            <div> {JSON.stringify(hoidanit)} & hoidanit update </div>
+            <div>{console.log("ERIC")}</div>
             <div className="child" style={{ borderRadius: "10px" }}>
                 child
             </div>
