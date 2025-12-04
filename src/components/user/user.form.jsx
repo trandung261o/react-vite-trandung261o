e25770a8f767057df79d752ea44692ee01a35d4e
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-04 23:13:36 +0700

    video 66

---
+++ src/components/user/user.form.jsx
@@ -0,0 +1,31 @@
+import { Button, Input } from "antd";
+
+const UserForm = () => {
+    return (
+        <div className="user-form" style={{ margin: "20px 0" }}>
+            <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+                <div>
+                    <span>Full name</span>
+                    <Input />
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input.Password />
+                </div>
+                <div>
+                    <span>Full name</span>
+                    <Input />
+                </div>
+                <div>
+                    <Button type="primary">Create User</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserForm;
