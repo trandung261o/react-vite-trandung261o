74b42f1e3e47955b35f207a5312f66d2f27a2861
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-04 23:55:25 +0700

    video 67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,27 +1,43 @@
 import { Button, Input } from "antd";
+import { useState } from "react";
 
 const UserForm = () => {
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [phone, setPhone] = useState("")
+
+    const handleClickBtn = () => {
+        console.log(">>> check state:", { fullName, email, password, phone });
+    }
+
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
                 <div>
                     <span>Full name</span>
-                    <Input />
+                    <Input value={fullName}
+                        onChange={event => setFullName(event.target.value)} />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input value={email}
+                        onChange={event => setEmail(event.target.value)} />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password value={password}
+                        onChange={event => setPassword(event.target.value)} />
                 </div>
                 <div>
-                    <span>Full name</span>
-                    <Input />
+                    <span>Phone</span>
+                    <Input value={phone}
+                        onChange={event => setPhone(event.target.value)} />
                 </div>
                 <div>
-                    <Button type="primary">Create User</Button>
+                    <Button type="primary" onClick={handleClickBtn}>
+                        Create User
+                    </Button>
                 </div>
             </div>
         </div>
